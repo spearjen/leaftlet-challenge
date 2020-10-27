29c0822cf6c527745f81c5e0a0faf8f8bfae4a94
Jay Spears <j.spears.denver@gmail.com>
2020-10-27 16:04:27 -0600

    adding missing index???

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -3,6 +3,7 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
+  console.log(data)
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
